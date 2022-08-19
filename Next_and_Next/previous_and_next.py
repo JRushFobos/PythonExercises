26f49e5d4d2106d7ea63f3c441b63cafe265edea
JRushFobos <fobos_media@mail.ru>
2022-08-19 15:24:27 +0500

    add num_sum and update previous and next

--- Next_and_Next/previous_and_next.py
+++ Next_and_Next/previous_and_next.py
@@ -1,8 +1,11 @@
 #Напишите программу, которая считывает целое число, 
 #после чего на экран выводится следующее и
 #предыдущее целое число с пояснительным текстом.
-
-a = input('Imput number')
-
-print('previous number of',a,'=', int(a)-1)
-print('next number of',a, '=', int(a)+1)
+while True:
+    a = input('Imput number')
+    if a == 'q':
+        print('Goodbay')
+        break
+    else:
+        print('previous number of',a,'=', int(a)-1)
+        print('next number of',a, '=', int(a)+1)
---
+++ Num_sum/num_sum.py
@@ -0,0 +1,13 @@
+def num_sum(number):
+    try:
+        int(number)
+        list_numbers=str(number)
+        summ=0
+        for list_number in list_numbers:
+            summ+=int(list_number)
+        print(summ)
+    except ValueError:
+        print ('«Это не целое число».')
+   
+num = input('введи число и я сложу его цифры')
+num_sum(num)
