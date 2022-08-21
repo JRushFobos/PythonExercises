34a9ede3856660ad5447a7fe24f998f17d584414
JRushFobos <fobos_media@mail.ru>
2022-08-21 20:47:37 +0500

    add Distance_iт_meters and Inevitability_itself

---
+++ Distance_iт_meters/Distance_in_meters.py
@@ -0,0 +1,3 @@
+centimeters = input('Input distance in centimeters')
+meters = float(centimeters)//100
+print(f'Distance in meters = {meters}')
---
+++ Inevitability_itself/inevitability_itself.py
@@ -0,0 +1,5 @@
+population = input('Enter the number of living creatures half of which Thanos wants to destroy')
+if int(population)/2==0:
+    print(f'number of creatures surviving after the destruction of half: {population}')
+else:
+    print (f'number of creatures surviving after the destruction of half + 1: {population}')
