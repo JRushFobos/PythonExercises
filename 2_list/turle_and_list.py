325fa530f113c8aa8fcaa8a12c4e40182377fe55
JRushFobos <fobos_media@mail.ru>
2022-10-05 22:47:10 +0500

    add turle and list

---
+++ 2_list/turle_and_list.py
@@ -0,0 +1,9 @@
+#объединить два списка в список кортежей
+a = ['a','b','c']
+b = [1,2,3]
+
+tuple1 = [(k,v) for k,v in zip(a,b)]
+print('кортеж', tuple1)
+
+dict1 = dict(zip(a,b))
+print('словарь',dict1)
