429d6d9b52f87bd45a38fe5d4f756d57ebc0fa5a
JRushFobos <fobos_media@mail.ru>
2022-10-05 10:07:58 +0500

    add def mul_to_int(a, b):

---
+++ mul_to_int/mul_to_int.py
@@ -0,0 +1,10 @@
+def mul_to_int(a, b):
+    res = a * b
+    if float(res).is_integer():
+        return int(res)
+    return res
+
+# Тесты
+print(mul_to_int(2, 4))
+print(mul_to_int(2.5, 4))
+print(mul_to_int(2.2, 2))
