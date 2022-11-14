e4b4d9df2f86840029e882c2613ac3a0fc1efe61
JRushFobos <fobos_media@mail.ru>
2022-11-14 23:50:26 +0500

    add a = int(input())

--- split_test/pos_abs.py
+++ split_test/pos_abs.py
@@ -4,10 +4,9 @@ os.chdir('f:\\LocalRepository\\PythonExercises\\split_test\\')
 with open('file_for_test.txt') as file_object:
    lines = file_object.readline()
 
-list_accounts = ''
-if int(lines)//3 == 0:
-   for line in lines:
-      list_accounts += line
+for line in lines:
+   if line.strip("\n") != "nickname_to_delete":
+      f.write(line)
 
 
 print(f'{list_accounts}')
